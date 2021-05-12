2adae86270211c690bab92a1bc049d5620c3bee2
pvmoore <1926576+pvmoore@users.noreply.github.com>
2021-05-12 20:09:08 +0100

    Add some TODOs

--- src/ppl4/version_.d
+++ src/ppl4/version_.d
@@ -11,10 +11,40 @@ Currently working on:
     Dot
     Constructor
     changing CastExpression to AsExpression
+    Constructor
+
+
 
 
 0.1.1 - Initial generation phase
 0.1.0 - Initial resolution phase
 0.0.1 - Initial commit to Github
+
+ */
+
+ /**
+    TODO
+    ###########################
+
+    - use get() and set() instead of [] indexing
+    - Casting. Use "as" or @cast(type, expr) ?
+    - Do we need assign/op eg. += since we won't support operator overloading for anything other
+      than ==
+    - No need for rol or ror
+
+    class {
+        a:int
+        b:int
+
+        init = fn(a, b) {
+            // automatically sets this.a = a; this.b = b
+        }
+        init = fn(a, b:int) {
+            // this.a = a
+            // b is local to this function
+        }
+
+    }
+
 
  */
